bb2d4e4349b1379844b60aea39489b23d544c2b9
Luigi Montanez <luigi.montanez@gmail.com>
2010-05-07 13:22:26 -0400

    Highlight entities when showing result content.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def formatted_content(result)
+    highlight(result.source_content.html_safe,
+              result.entities.map {|e| e.name },
+              :highlighter => '<span class="highlight">\1</span>')
+  end
+
 end
