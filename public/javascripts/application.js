5b039575e342fd53d6c420de68bd8cc1cbe93bcd
Luigi Montanez <luigi.montanez@gmail.com>
2010-05-07 14:39:19 -0400

    Add text field to copy the result's link.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,5 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function() {
+  
+  $("input#share_url").click(function() {$(this).select()});
+  
+});
