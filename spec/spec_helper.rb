3e33f9b0f7174daafd489e19aedde61c228833d2
Luigi Montanez <luigi.montanez@gmail.com>
2010-05-24 16:18:45 -0400

    Merge acceptance_helper into spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,15 @@
 ENV["RAILS_ENV"] ||= 'test'
+
 require File.dirname(__FILE__) + "/../config/environment" unless defined?(Rails)
 require 'rspec/rails'
+require "steak"
+require 'capybara/rails'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 Rspec.configure do |config|
+  config.include Capybara
   config.mock_with :rr
   # config.use_transactional_examples = false
 end
+
+Capybara.default_selector = :css
