1ea4af9ec84775298a052b05b2bc44651f9d95ae
Luigi Montanez <luigi.montanez@gmail.com>
2010-08-05 16:08:23 -0400

    Add URL slug to feedback email [#4542585]

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,6 +2,7 @@ class Notifier < ActionMailer::Base
 
   def feedback_email(feedback)
     @message = feedback.message
+    @slug = feedback.slug
     mail :from => "#{feedback.name} <#{feedback.email}>", 
          :to => 'poligraft@sunlightfoundation.com',
          :subject => "Poligraft Feedback"
